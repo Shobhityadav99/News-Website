ce502d0bc382cb2c2cf6cd2b49ee9d8bf70562b8
Shobhit Yadav <shobhit999000@gmail.com>
2021-02-13 16:19:56 +0530

    Api linked

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-newsAccordion=document.getElementById('newsAccordion');
-const xhr=new XMLHttpRequest();
+newsAccordion = document.getElementById('newsAccordion');
+let apiKey = 'c4c10316899241c88721904ea2b93a3d'
+let sources = 'TIMESOFINDIA.COM';
 
-
-let news = `<div class="accordion-item">
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `http://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`, true);
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        let newsHtml ="";
+        for (news in articles) {
+            console.log(articles[news]);
+            let news = `<div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -11,13 +20,12 @@ let news = `<div class="accordion-item">
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
                     data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is hidden by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does limit overflow.
-                    </div>
+                    <div class="accordion-body"> </div>
                 </div>
-            </div>`
+                </div>`
+                newsHtml+=news;
+        }
+        console.log(articles);
+    }
+}
+xhr.send();
